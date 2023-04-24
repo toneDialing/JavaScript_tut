608cb966bf2e1154ebdccbeaf1d992fbc6831e48
Matthew Rathkey <rathkeym@gmail.com>
2023-04-24 17:05:01 -0600

    Auto-increment counter once per second

--- counter.js
+++ counter.js
@@ -4,13 +4,10 @@ function count()
 {
     counter++;
     document.querySelector('h1').innerHTML = counter;
-
-    if(counter%10===0)
-    {
-        alert(`Counter is now ${counter}!`);
-    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = count;
+
+    setInterval(count, 1000);
 });
